568a93fe55655b1bf46464233492516d4f3caaec
IVydraFiuba <ivydra@fi.uba.ar>
2024-07-07 19:09:45 -0300

    Agrego un endpoint para poder agregar lo basico de los usuarios a la base de datos

--- juegoTp/backend/main.py
+++ juegoTp/backend/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config.from_object(Desarrollo) 
 CORS(app)
 
-@app.route('usuarios',methods=["GET"]) #Por defauld es GET pero no esta mal ponerlo
+@app.route('/usuarios',methods=["GET"]) #Por defauld es GET pero no esta mal ponerlo
 def data_usuarios():
     try:
         usuarios = Usuario.query.all()
@@ -28,6 +28,36 @@ def data_usuarios():
     except:
         return jsonify({'success':False,"mensaje":"No tenemos usuarios cargados"}),409
 
+@app.route('/usuarios', methods=["POST"])
+def nueva_partida():
+    try:
+        formulario_data =request.json
+        Nom_usuario = formulario_data.get("nom_usuario")
+        Nom_concesionaria = formulario_data.get("nom_concesionaria")
+        #Aca si es necesario podria hacer validaciones de los datos que me paso el usuario
+        #
+        #
+        nueva_concesionaria = Concesionaria(Nom_concesionaria=Nom_concesionaria)
+        db.session.add(nueva_concesionaria)
+        db.session.flush()
+        nuevo_usuario = Usuario(nom_usuario=Nom_usuario, concesionaria_id=nueva_concesionaria.id)
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+
+        #Una buena practica es retornar lo que registramos
+        return jsonify({
+            'success':True,
+            'Usuario':{'Id':nuevo_usuario.id ,
+                        'Nombre':nuevo_usuario.nom_usuario,
+                        'Concesionaria':{'Id': nueva_concesionaria.id,
+                                    'Nombre': nueva_concesionaria.nom_concesionaria,
+                                    },
+                        'Plata': nuevo_usuario.plata}
+            }) 
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({'success':False,'message':'No se pudo crear el usuario'}),500
 
 if __name__ == '__main__':
     db.init_app(app)
