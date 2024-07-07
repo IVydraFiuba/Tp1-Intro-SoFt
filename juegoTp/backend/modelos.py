b4c3eaeb07ddac053f81cc44465510aa44b8ba90
IVydraFiuba <ivydra@fi.uba.ar>
2024-07-07 18:51:38 -0300

    Agrego lo basico de unos modelos de la base de datos

--- juegoTp/backend/modelos.py
+++ juegoTp/backend/modelos.py
@@ -2,5 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db=SQLAlchemy()
 
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nom_usuario = db.Column(db.String(255), nullable=False)
+    concesionaria_id = db.Column(db.Integer, db.ForeignKey('concesionarias.id'), nullable=False)
+    plata = db.Column(db.Integer, nullable=False , default=500)
+
+class Concesionaria(db.Model):
+    __tablename__ = 'concesionarias'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nom_concesionaria = db.Column(db.String(255), nullable=False)
+    
 #sirve para que pueda importar usando el * enves de poner todos los nombres
-__all__ = ['db','','']
+__all__ = ['db','Usuario','Concesionaria']
