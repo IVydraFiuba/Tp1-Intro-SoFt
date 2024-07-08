9b11e42b0c6ef9526648f8317a295a3168361c3c
IVydraFiuba <ivydra@fi.uba.ar>
2024-07-08 12:02:22 -0300

    Agrego una pagina para cada usuario

---
+++ juegoTp/frontend/concesionaria/main.js
@@ -0,0 +1,17 @@
+let parametros = window.location.search;
+let id = new URLSearchParams(parametros).get("id")
+
+localStorage.setItem('id',id);
+
+fetch("http://localhost:5000/usuarios/"+id)
+            .then((respuesta) => respuesta.json())
+            .then(cargar_datos)
+            .catch((error) => console.log("ERROR", error))
+
+            function cargar_datos(contenido){
+                const contenedor_nombre = document.getElementById("Nombre_concesionaria")
+                contenedor_nombre.innerText = contenido.Nombre
+                const contenedor_plata = document.getElementById("Plata_usuario")
+                contenedor_plata.innerText = contenido.Plata
+                
+            }
